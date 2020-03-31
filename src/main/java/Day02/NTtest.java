23024f9337a74b40328e0542bb5e69b4a73cfbf0
zengjf <zengjf@jiguang.cn>
2020-03-31 22:43:45 +0800

    everyday2

---
+++ src/main/java/Day02/NTtest.java
@@ -0,0 +1,19 @@
+package Day02;
+
+/**
+ * Author:Jude
+ * Date:2020-03-31 10:41 PM
+ */
+/*
+   \n : 换行符
+
+	\t : 制表符 (aaa   ccc)
+
+ */
+public class NTtest {
+    public static void main(String[] args) {
+        System.out.println("aaaa\nbbbb");
+        System.out.println("aa\tbb");
+    }
+
+}
