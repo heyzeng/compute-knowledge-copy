13234d80f4ca13f1d8bd166ec30fa78c2abf377f
heyzeng <270806204@qq.com>
2020-04-23 10:03:06 +0800

    day03

---
+++ src/main/java/Day03/ExerTest.java
@@ -0,0 +1,20 @@
+package Day03;
+
+/*
+		后++ ：先运算（赋值）再自加1
+		前++ ：先自加1  再运算（赋值）
+		后-- ：先运算（赋值）再自减1
+		前-- ：先自减1  再运算（赋值）
+*/
+public class ExerTest {
+    public static void main(String[] args) {
+
+        int x = 1;
+        int y=1;
+
+        if(x++==2 & ++y==2){
+            x =7;
+        }
+        System.out.println("x="+x+",y="+y);
+    }
+}
--- src/main/java/Day03/ifTest.java
+++ src/main/java/Day03/ifTest.java
@@ -3,11 +3,12 @@ package Day03;
 
 public class ifTest {
     public static void main(String[] args) {
+
         int age = 10;
-        if (age >= 10){
+
+        if(age > 10) {
             System.out.println("good");
-        }
-        else {
+        }else {
             System.out.println("bad");
         }
     }
