0be138b0a2c81772d9817d053d62f6842a3806ec
heyzeng <270806204@qq.com>
2020-04-30 16:06:57 +0800

    day04-8

---
+++ src/main/java/Day04/stwichTest.java
@@ -0,0 +1,46 @@
+package Day04;
+
+/*
+	分支结构 ：switch-case
+
+	格式：
+
+		switch(表达式){
+
+		case 常量1：
+			执行语句1;
+			break;
+		case 常量2:
+			执行语句2;
+			break;
+
+		......
+		default:
+			执行语句n;
+			break;
+		}
+
+		说明：
+			1.根据表达式和case后面的值进行匹配，一旦匹配成功则执行case中的执行语句，
+					直到break或switch-case结尾结束（执行到switch-case中的最后一行代码）。
+			2.break：用来结束case语句的。可以省略。
+			3.default :如果case都没有匹配成功，则执行default中的执行语句。default的位置是任意的
+					default可以省略。
+			4.case子句中的值必须是常量，且所有case子句中的值应是不同的
+			5.表达式的类型: byte short int char String(jdk1.7) 枚举
+*/
+public class stwichTest {
+    public static void main(String[] args) {
+        switch (1){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("other");
+                break;
+        }
+    }
+}
---
+++ src/main/java/Day04/whileTest.java
@@ -0,0 +1,41 @@
+package Day04;
+/*
+
+
+	循环结构 ：while
+
+	循环的四要素：
+		1.初始化条件
+		2.循环条件
+		3.循环体
+		4.迭代条件
+
+	格式：
+		初始化条件：
+		while(循环条件){
+			循环体;
+			迭代条件;
+		}
+
+*/
+public class whileTest {
+    public static void main(String[] args) {
+        //需求:求100以内的偶数，求偶数的个数，求偶数的总和？
+        int sum = 0; //和
+        int count = 0; //计数
+        int i = 1; //初始化条件
+
+        while (i <= 100){//循环条件
+
+            if (i % 2 == 0 ) {//循环体
+                count++;
+                sum +=i; //sum = sum +i
+                System.out.println(i);
+            }
+            i ++;//迭代条件
+
+            System.out.println("sum = " + sum + " count = " + count);
+
+        }
+    }
+}
