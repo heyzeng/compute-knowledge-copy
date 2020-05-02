5630592b0ce0ffa8fc5e185bc8416ca4a8d16634
heyzeng <270806204@qq.com>
2020-05-02 23:37:17 +0800

    day05-04

--- Java/Day05/src/main/java/doWhileTest02.java
+++ Java/Day05/src/main/java/doWhileTest02.java
@@ -1,3 +1,4 @@
+
 /*
 
 
@@ -6,5 +7,19 @@
 		do-while的循环体至少执行一次。
 
 */
+
 public class doWhileTest02 {
+    public static void main(String[] args) {
+
+        boolean boo = false;
+
+        while(boo){
+            System.out.println("while");
+        }
+
+        do{
+            System.out.println("do - while");
+        }while(boo);
+
+    }
 }
